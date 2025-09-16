436d0f3f3ebd065133e57128b59a71ce2a8b5929
Dmytro Basamyhin <dima17lit@gmail.com>
2025-09-17 01:10:25 +0300

    fix

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -7,10 +7,12 @@ export default interface NotesHttpResponse {
     totalPages: number
 }
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  withCredentials: true
-})
+  baseURL,
+  withCredentials: true,
+});
 
 export interface Credentials {
   email: string,
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ["ac.goit.global"],
     remotePatterns: [
       {
         protocol: "https",
